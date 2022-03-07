012a609c7eed99d0b333a10bc85b0ce596672ecf
doilz <dotteda@gmail.com>
2022-03-07 22:28:19 +0900

    Error Fix

--- controllers/BlockApi.js
+++ controllers/BlockApi.js
@@ -1,33 +1,51 @@
 const asyncHandler = require("../middleware/async");
-const request = require("request");
+var request = require("request");
 const ErrorResponse = require('../utils/errorResponse');
 const dotenv = require("dotenv");
-const router = require("../routes/testApi");
 dotenv.config({ path: './config/.env'}); 
-const USER = process.env.USER;
-const PASSWORD = process.env.PASSWORD;
-const LOCALHOST = process.env.LOCAL_RPC;
-const PORT = process.env.PORT;
-const ID_STRING = process.env.ID_STRING;
-const account = process.env.ACCOUNT;
+const USER = "kbpark";
+const PASSWORD = 1234;
+const PORT = 3001;
+const RPC_PORT = 9776;
 const headers = {
     'Content-Type': 'text/plain',
 };
+const ID_STRING = "kigacoin_id";
+console.log(`${USER} ${PASSWORD} 127.0.0.1 ${RPC_PORT}`.blue);
 
-router.get("/getblock", (req, res, next) => {
-    
-})
+// exports.getBlock = function (req, res, next)  {
+//     const blockHash = req.body.hash;
+//     console.log("BlockHash: " + blockHash);
+//     var dataString = `{"jsonrpc":"1.0","id":"kbpark","method":"getblock","params":["${blockHash}"]}`;
+//     var options = {
+//         url: `http://${USER}:${PASSWORD}@127.0.0.1:${RPC_PORT}/`,
+//         method: 'POST',
+//         headers: headers,
+//         body: dataString   
+//     };
 
-exports.getBlock = asyncHandler(async (req, res, next) => {
-    const blockHash = req.params.hash;
-    console.log(blockHash.red.bold);
-    var dataString = `{"jsonrpc":"1.0","id":"kbpark","method":"getblock","params":[${blockHash}]}`;
-    var url = `http://${USER}:${PASSWORD}@${LOCALHOST}:${PORT}/`;
+//     callback = function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             console.log("body:" + body);
+//             const data = JSON.parse(body);
+//             console.log("data" + data);
+//             res.status(200).json({ success: true, data: data });
+//         }
+//     }
+//     request (options, callback);
+// };
+
+// GET
+
+exports.getBlock = function (req, res, next)  {
+    const blockHash = req.query.hash;
+    console.log("BlockHash: " + blockHash);
+    var dataString = `{"jsonrpc":"1.0","id":"kbpark","method":"getblock","params":["${blockHash}"]}`;
     var options = {
-        url: url,
+        url: `http://${USER}:${PASSWORD}@127.0.0.1:${RPC_PORT}/`,
         method: 'POST',
         headers: headers,
-        body: dataString
+        body: dataString   
     };
 
     callback = function (error, response, body) {
@@ -35,19 +53,22 @@ exports.getBlock = asyncHandler(async (req, res, next) => {
             console.log("body:" + body);
             const data = JSON.parse(body);
             console.log("data" + data);
-            res.status(200).json({ success: true, data: body });
+            res.status(200).json({ success: true, data: data });
         }
     }
-    request(options, callback);
-});
+    request (options, callback);
+};
 
-exports.getBlockHash = asyncHandler(async (req, res, next) => {
-    const index = req.params.index;
-    console.log("Searching block index : " + blockIndex.red.bold);
+
+
+
+exports.getBlockHash = function (req, res, next)  {
+    const index = req.body.index;
+    console.log("Searching block index : " + index);
     var dataString = `{"jsonrpc":"1.0","id":"kbpark","method":"getblockhash","params":[${index}]}`;
-    var url = `http://${USER}:${PASSWORD}@${LOCALHOST}:${PORT}/`;
+    
     var options = {
-        url: url,
+        url: `http://${USER}:${PASSWORD}@127.0.0.1:${RPC_PORT}/`,
         method: 'POST',
         headers: headers,
         body: dataString
@@ -57,8 +78,30 @@ exports.getBlockHash = asyncHandler(async (req, res, next) => {
         if (!error && response.statusCode == 200) {
             console.log(body);
             const data = JSON.parse(body);
-            res.status(200).json({ success: true, data: data });
+            res.send(data);
         }
     }
     request (options, callback);
-}); 
+}; 
+
+
+exports.getBlockCount =function (req, res, next)  {
+    console.log("Searching block count");
+    var dataString =  `{"jsonrpc":"1.0","id":"${ID_STRING}","method":"getblockcount","params":[]}`;
+   
+    var options = {
+        url: `http://${USER}:${PASSWORD}@127.0.0.1:${PORT}/`,
+        method: 'POST',
+        headers: headers,
+        body: dataString
+    };
+
+    callback = function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            const data = JSON.parse(body);
+            res.status(200).json({ success: true, data: data, body });
+        }
+    }
+    request (options, callback);
+};
